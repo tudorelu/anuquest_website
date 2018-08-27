18f9953c6c9d3ffdfdc84b764163d104d91966db
Karman Sawhney <u5829181@anu.edu.au>
2018-08-27 16:59:27 +1000

    added the code for including a video

--- Website/anuQuest.js
+++ Website/anuQuest.js
@@ -1,12 +1,29 @@
 
 $(function () {
-    $(window).on("load resize", function () {
-        $(".fill-screen").css("height", window.innerHeight);
-    });
-
-    // add Bootstrap's scrollspy
-    $('body').scrollspy({
-        target: '.navbar',
-        offset: 160
-    });
+  $(window).on("load resize", function () {
+    $(".fill-screen").css("height", window.innerHeight);
+    // autoplays the video on load.
+    var video = $("#frontVideo")[0];
+    video.play();
+    debugger;
+    // For front background
+    // the video does not work for mobiles and will only show the image
+    // cannot use youtube links due to youtube T&Cs
+    // $(".frontBackground").background({
+    //   source: {
+    //     poster: "../../images/lake.jpg",
+    //     // mp4: "../../images/bikes.mp4"
+    //     video: "https://www.youtube.com/embed/TBa4PoXqlPo"
+    //     // ,
+    //     // webm: "http://example.com/video.webm",
+    //     // mp4: "http://example.com/video.mp4",
+    //     // ogg: "http://example.com/video.ogv"
+    //   }
+    // });
+  });
+  // add Bootstrap's scrollspy
+  $('body').scrollspy({
+    target: '.navbar',
+    offset: 160
+  });
 });
